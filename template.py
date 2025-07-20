20eb403ed2e7c0ea4ace009a7ec18ff5d428a9e6
sarth-04 <sarthakingle04@gmail.com>
2025-07-20 08:35:07 +0530

    Fix formatting issues in file paths and improve logging messages in template.py and requirement.txt

--- template.py
+++ template.py
@@ -13,8 +13,8 @@ list_of_files = [
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",    
     f"src/{project_name}/utils/common.py",
-    f"src/{project_name}/logger/__init__.py",
-    f"src/{project_name}/config/__init__.py",
+    f"src /{project_name}/logger/__init__.py",
+    f"src /{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",  
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
@@ -35,14 +35,15 @@ for filepath in list_of_files:
         
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for the file {filename}")
-
+        logging.info(f"Creating directory: {filedir}for the file {filename}")
+            
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, 'w') as f:
             pass
-        logging.info(f"Creating empty file: {filepath}")
+            logging.info(f"Creating empty file: {filename}")
+        
     else:
-        logging.info(f"File already exists: {filepath}")
+        logging.info(f"File already exists: {filename}")
                          
     
 
